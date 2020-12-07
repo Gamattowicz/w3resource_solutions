044b57418c7f671721296bf49beeac22d62aecce
Gamattowicz <p.romanczuk31@gmail.com>
2020-12-07 21:05:37 +0100

    add new exercises

---
+++ Python Data Type List/12[unsolved].py
@@ -0,0 +1,9 @@
+''' Python 3.9
+7.12.2020
+
+Write a Python program to count the occurrences of each word in a given
+sentence.'''
+
+
+
+
---
+++ Python Data Type List/13.py
@@ -0,0 +1,14 @@
+''' Python 3.9
+7.12.2020
+
+Write a Python script that takes input from the user and displays that input
+back in upper and lower cases.'''
+
+
+def main():
+    s = input('Say something: ')
+    print(s.upper())
+    print(s.lower())
+
+
+main()
